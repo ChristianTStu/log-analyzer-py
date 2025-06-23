a59ac3839eb80ebc9bbd1fbaee9828b12175a3f9
Christian Stuart <christiantstu@gmail.com>
2025-06-23 08:58:48 -0400

    Modularized log analysis by moving detection logic to brute_force.py

--- analyzer.py
+++ analyzer.py
@@ -1,21 +1,4 @@
-import re
-
-
-def analyze_log(path, threshold=3, label=""):
-    easy_log = open(path, "r")
-    failed_ip_counts = {}
-    for line in easy_log:
-        if "Failed password" in line:
-            ip_matches = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", line)
-            if ip_matches:
-                ip = ip_matches[0]
-                if ip in failed_ip_counts:
-                    failed_ip_counts[ip] += 1
-                else:
-                    failed_ip_counts[ip] = 1
-    for ip, count in failed_ip_counts.items():
-        if count > 3:
-            print(f"[!] Inside of {label} log report, {ip} had {count} failed login attempts")
+from detections.brute_force import analyze_log
 
 analyze_log("sample_logs/auth_easy.log", label="[EASY]")
 analyze_log("sample_logs/auth_medium.log", label ="[MEDIUM]")
---
+++ detections/brute_force.py
@@ -0,0 +1,17 @@
+import re
+
+def analyze_log(path, threshold=3, label=""):
+    easy_log = open(path, "r")
+    failed_ip_counts = {}
+    for line in easy_log:
+        if "Failed password" in line:
+            ip_matches = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", line)
+            if ip_matches:
+                ip = ip_matches[0]
+                if ip in failed_ip_counts:
+                    failed_ip_counts[ip] += 1
+                else:
+                    failed_ip_counts[ip] = 1
+    for ip, count in failed_ip_counts.items():
+        if count > 3:
+            print(f"[!] Inside of {label} log report, {ip} had {count} failed login attempts")
